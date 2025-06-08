b760e8e9bbeb512c6dd8f9b7645fac0086cf4a36
sanchuanhehe <2947512113@qq.com>
2025-06-08 21:31:54 +0800

    将 TIM1 中断处理程序更改为 TIM6，更新相关配置以支持 TIM6 作为时间基准源

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -275,7 +275,7 @@ void MPU_Config(void)
 
 /**
   * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM1 interrupt took place, inside
+  * @note   This function is called  when TIM6 interrupt took place, inside
   * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
   * a global variable "uwTick" used as application time base.
   * @param  htim : TIM handle
@@ -286,7 +286,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   /* USER CODE BEGIN Callback 0 */
 
   /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM1)
+  if (htim->Instance == TIM6)
   {
     HAL_IncTick();
   }
